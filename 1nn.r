61e50c778dbe2bd11c5c9bc64471edeb2fdccdec
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2018-11-14 19:10:54 +0300

    Update 1nn.r

--- 1nn.r
+++ 1nn.r
@@ -25,18 +25,20 @@ NN<-function(xl, z)
 colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue") 
 X<-sample(c(1:150), 25, replace=TRUE) 
 xl <- iris[X, 3:5] 
-plot(iris[X, 3:4], pch = 21, bg = colors[xl$Species], col = colors[xl$Species],asp=1) 
-a<-0 
-b<-0 
-while (a<7) 
+plot(iris[X, 3:4], pch = 21, bg = colors[xl$Species], col = colors[xl$Species], asp = 1, 
+     xlab = "Длина Лепестка", ylab = "Ширина Лепестка", main = "Карта классификации для фунции 1NN") 
+
+x <- 0.6 
+y <- 0 
+while (x<7) 
 { 
-  while (b<3) 
+  while (y<3) 
   { 
-    z <- c(a, b) 
+    z <- c(x, y) 
     class <- NN(xl, z ) 
     points(z[1], z[2], pch = 1, bg = colors[class], col=colors[class] ) 
-    b<- b+0.1
+    y <- y+0.1
   } 
-  b<-0 
-  a<- a+0.1 
+  y <- 0 
+  x <- x+0.1 
 }
